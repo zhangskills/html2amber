d736aefc2d44293f20648ba70772e16c759906af
Jack.Zhang <zhangmj@eicodesign.com>
2014-09-19 18:32:31 +0800

    添加ace.js、bootstrap库

--- js/index.js
+++ js/index.js
@@ -1,75 +1,100 @@
-var str2dom = function(str) {
+$(function() {
 
-};
+	var editor = ace.edit("editor");
+	// editor.setTheme("ace/theme/monokai");
+	editor.getSession().setMode("ace/mode/html");
 
+	var resultE = ace.edit('result')
+	// resultE.setTheme("ace/theme/monokai");
+	resultE.getSession().setMode("ace/mode/jade");
+	resultE.setReadOnly(true);
 
-var convert = function() {
-	var html = $('#editor').val();
-	e = $(html)
-	var s = show(e)
-	console.log(s)
-};
+	var convert = function() {
+		var html = editor.getValue();
+		e = $(html);
+		var s = show(e);
 
-var getTagContent = function(e) {
-	return e.html()
-}
-var getPrefixTab = function(num) {
-	var s = ''
-	for (var i = 0; i < num; i++) {
-		s += '\t'
-	}
-	return s
-}
+		resultE.setValue(s)
+	};
 
-var show = function(e, depth) {
-	if (!depth) {
-		depth = 0
-	}
-	var s = ''
-	e.each(function() {
-		var t = $(this)
-		var tagName = t.get(0).tagName
-		s += getPrefixTab(depth)
-		if (tagName) {
-			s += tagName
+	var getPrefixTab = function(num) {
+		var s = ''
+		for (var i = 0; i < num; i++) {
+			s += '\t'
 		}
-		if (t.get(0).childElementCount > 0) {
-			s += show(t.contents(), depth + 1)
-		} else {
-			var lines = t.get(0).textContent.split('\n')
-			var rLines = []
-			for (var i = 0; i < lines.length; i++) {
-				var line = lines[i].replace(/(^\s+)|(\s+$)/g, '')
-				if (line) {
-					rLines.push(line)
-				}
+		return s
+	}
+
+	var getTagContentLines = function(e) {
+		var lines = e.get(0).textContent.split('\n')
+		var rLines = []
+		for (var i = 0; i < lines.length; i++) {
+			var line = lines[i].replace(/(^\s+)|(\s+$)/g, '')
+			if (line) {
+				rLines.push(line)
 			}
-			// console.log(tagName, t.get(0).textContent, lines, rLines)
-			if (rLines.length == 1) {
-				if (tagName) {
-					s += '\t' + rLines[0]
-				} else {
-					s += '\n' + getPrefixTab(depth) + '| ' + rLines[0]
-				}
-			} else {
-				for (var i = 0; i < rLines.length; i++) {
-					var line = rLines[i]
-					if (line) {
-						s += '\n'
-						if (tagName) {
-							s += '\t'
-						}
-						s += getPrefixTab(depth) + '| ' + line
+		}
+		return rLines
+	};
+	var getTagAttr = function(e) {
+		var s = '';
+		if (e.get(0).attributes.length > 0) {
+			var attrs = e.get(0).attributes;
+			for (var i = 0; i < attrs.length; i++) {
+				var attrName = attrs[i].name;
+				var attrVals = attrs[i].value.split(/\s+/);
+				for (var j = 0; j < attrVals.length; j++) {
+					var attrVal = attrVals[j]
+					if (attrVal) {
+						s += '[' + attrName + '="' + attrVal + '"]'
 					}
 				}
 			}
-			s+='\n'
 		}
-	})
-	return s
-}
+		return s;
+	};
 
-$(function() {
-	var html = ""
-	convert()
+
+	var show = function(e, depth) {
+		if (!depth) {
+			depth = 0
+		}
+		var s = ''
+		e.each(function() {
+			var t = $(this)
+			var prefixTab = getPrefixTab(depth)
+			var tagName = t.get(0).tagName
+			s += prefixTab
+			if (tagName) {
+				s += tagName.toLowerCase() + getTagAttr(t)
+			}
+			if (t.get(0).childElementCount > 0) {
+				s += show(t.contents(), depth + 1)
+			} else {
+				var lines = getTagContentLines(t)
+				if (lines.length == 1) {
+					if (tagName) {
+						s += ' ' + lines[0]
+					} else {
+						s += '\n' + prefixTab + '| ' + lines[0]
+					}
+				} else {
+					for (var i = 0; i < lines.length; i++) {
+						var line = lines[i]
+						if (line) {
+							s += '\n'
+							if (tagName) {
+								s += '\t'
+							}
+							s += prefixTab + '| ' + line
+						}
+					}
+				}
+				s += '\n'
+			}
+		})
+		return s
+	}
+
+	$('#handle').click(convert);
 });
